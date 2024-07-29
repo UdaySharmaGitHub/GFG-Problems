990bf1540a9052febbc5575547dc0cfc64c732a5
Uday <udaysharma4600@gmail.com>
2024-07-29 13:24:21 +0530

    POTD 29-Jul-2024

--- Medium Problems/Row_with_max_1s.cpp
+++ Medium Problems/Row_with_max_1s.cpp
@@ -1,8 +1,6 @@
 /*
 Given a boolean 2D array of n x m dimensions where each row is sorted. Find the 0-based index of the first row that has the maximum number of 1's.
-
 Example 1:
-
 Input: 
 N = 4 , M = 4
 Arr[][] = {{0, 1, 1, 1},
@@ -12,32 +10,21 @@ Arr[][] = {{0, 1, 1, 1},
 Output: 2
 Explanation: Row 2 contains 4 1's (0-based
 indexing).
-
 Example 2:
-
 Input: 
 N = 2, M = 2
 Arr[][] = {{0, 0}, {1, 1}}
 Output: 1
 Explanation: Row 1 contains 2 1's (0-based
 indexing).
-
 Your Task:  
 You don't need to read input or print anything. Your task is to complete the function rowWithMax1s() which takes the array of booleans arr[][], n and m as input parameters and returns the 0-based index of the first row that has the most number of 1s. If no such row exists, return -1.
- 
-
 Expected Time Complexity: O(N+M)
 Expected Auxiliary Space: O(1)
-
-
 Constraints:
 1 ≤ N, M ≤ 103
 0 ≤ Arr[i][j] ≤ 1 
 */
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
 // } Driver Code Ends
 //User function template for C++
 class Solution{
@@ -57,25 +44,3 @@ public:
 	}
 
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector< vector<int> > arr(n,vector<int>(m));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin>>arr[i][j];
-            }
-        }
-        Solution ob;
-        auto ans = ob.rowWithMax1s(arr, n, m);
-        cout << ans << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
