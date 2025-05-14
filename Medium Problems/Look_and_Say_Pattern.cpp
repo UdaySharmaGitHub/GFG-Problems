55d27247596267c258ebc852ac7fc1e205478cab
Uday <udaysharma4600@gmail.com>
2025-05-14 15:23:33 +0530

    14-May-2025

--- Medium Problems/Look_and_Say_Pattern.cpp
+++ Medium Problems/Look_and_Say_Pattern.cpp
@@ -15,41 +15,55 @@ To generate a member of the sequence from the previous member, read off the digi
 21 is read off as "one 2, then one 1" or 1211.
 1211 is read off as "one 1, one 2, then two 1s" or 111221.
 111221 is read off as "three 1s, two 2s, then one 1" or 312211.
-Example 1:
+Examples:
 
-Input:
-n = 5
+Input: n = 5
 Output: 111221
-Explanation: The 5th row of the given pattern
-will be 111221.
-Example 2:
-
-Input:
-n = 3
+Explanation: The 5th row of the given pattern will be 111221.
+Input: n = 3
 Output: 21
-Explanation: The 3rd row of the given pattern
-will be 21.
-
-Your Task:  
-You dont need to read input or print anything. Complete the function lookandsay() which takes integer n as input parameter and returns a string denoting the contents of the nth row of given pattern.
-
-
-Expected Time Complexity: O(2n)
-Expected Auxiliary Space: O(2n-1)  
-
+Explanation: The 3rd row of the given pattern will be 21.
 
 Constraints:
 1 ≤ n ≤ 30
 */
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h> 
-using namespace std;
-
-// } Driver Code Ends
 //User function template for C++
 
+//------------------APPROACH-1---------------------------///
+
+class Solution {
+  public:
+    string countAndSay(int n) {
+        // code here
+        string ans = "1" , res = "";
+        if(n==1) return ans;
+        for(int i=1;i<n;i++){
+            stack<char> st;
+            for(char ch:ans){
+                if(st.empty()) st.push(ch);
+                else{   if(st.top() != ch){
+                            res+=('0'+st.size());
+                            res+=st.top();
+                            st = stack<char>();
+                        }
+                        st.push(ch);
+
+                }
+            }
+            if(!st.empty()){
+                            res+=('0'+st.size());
+                            res+=st.top();
+                            st = stack<char>();
+            }
+            ans= res;
+            res ="";
+        }
+        return ans;
+    }
+};
+
+
+//------------------APPROACH-2---------------------------///
 class Solution
 {
   public:
@@ -102,18 +116,3 @@ class Solution
 	return str;
     }   
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        Solution obj;
-        cout<<obj.lookandsay(n)<<endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
