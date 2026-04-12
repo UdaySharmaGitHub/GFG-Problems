082209e60418190f17bc7b2bd6350d5216bf3bf2
Uday Sharma <udaysharmaprojects+git@gmail.com>
2026-04-12 09:11:34 +0530

    POTD 12-April-2026

--- Easy Problems/Toeplitz_matrix.cpp
+++ Easy Problems/Toeplitz_matrix.cpp
@@ -26,40 +26,11 @@ Constraints:
 Expected time complexity: O(n*m)
 Expected space complexity: O(1)
 */
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-bool isToeplitz(vector<vector<int>> &mat);
-
-int main() {
-    // your code goes here
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> a(n, vector<int>(m, 0));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++)
-                cin >> a[i][j];
-        }
-
-        bool b = isToeplitz(a);
-
-        if (b == true)
-            cout << "true";
-        else
-            cout << "false";
-
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
-
-
+/*
+    Using Matrix
+    Time Complexity: o(M*N);
+    Space Complexity: O(1);
+*/
 
 bool isToeplitz(vector<vector<int>>& mat) {
     // code here
