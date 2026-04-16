e5a44664a619efad559c4ab5e5cca26512cc1937
Uday Sharma <udaysharmaprojects+git@gmail.com>
2026-04-16 23:55:57 +0530

    POTD 16-April-2026

--- Medium Problems/Implement_Atoi.cpp
+++ Medium Problems/Implement_Atoi.cpp
@@ -32,38 +32,22 @@ Constraints:
 */
 //{ Driver Code Starts
 //Initial template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-//User function template for C++
-
-class Solution{
+class Solution {
   public:
-    /*You are required to complete this method */
-    int atoi(string s) {
-        //Your code here
-        for(int i=0;i<s.size();i++){
-            if(isalpha(s[i]) ||  (s[i]=='-' && i>0)){
-                return -1;
-            }
+    int myAtoi(string s) {
+        // Your code here
+          // code here
+        int sign = 1, r = 0, i = 0;
+        while(s[i] == ' ')      i++;        // ignore whitespaces
+        if(s[i] == '-' || s[i] == '+'){
+            if(s[i++] == '-')       sign = -1;
         }
-        int n = stoi(s);
-        return n;
+        while(s[i] >= '0' && s[i] <= '9'){ 
+            if(r > INT_MAX/10 || r == INT_MAX/10 && s[i]-'0' > 7)     
+                return      sign == 1 ? INT_MAX : INT_MIN;
+            r = 10*r+(s[i++]-'0');
+        }
+        return  r*sign;
+       
     }
 };
-
-//{ Driver Code Starts.
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		string s;
-		cin>>s;
-		Solution ob;
-		cout<<ob.atoi(s)<<endl;
-	}
-}
-// } Driver Code Ends
