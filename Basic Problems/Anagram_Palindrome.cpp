11e4a161bdec814388f84db520eaa9c1b69ed4fc
Uday Sharma <udaysharmaprojects+git@gmail.com>
2026-04-17 23:59:01 +0530

    POTD 17-April-2026

--- Basic Problems/Anagram_Palindrome.cpp
+++ Basic Problems/Anagram_Palindrome.cpp
@@ -2,15 +2,18 @@
 Anagram Palindrome
 Given a string s, determine whether its characters can be rearranged to form a palindrome. Return true if it is possible to rearrange the string into a palindrome; otherwise, return false.
 Examples
+
 Input: s = "baba"
 Output: true
 Explanation: Can be rearranged to form a palindrome "abba" 
+
 Input: s = "geeksogeeks"
 Output: true
 Explanation: The characters of the string can be rearranged to form the palindrome "geeksoskeeg".
 Input: s = "geeksforgeeks"
 Output: false
 Explanation: The given string can't be converted into a palindrome.
+
 Constraints:
 1 ≤ s.length ≤ 106
 s consists of only lowercase English letters.
