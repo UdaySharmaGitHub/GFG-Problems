08ff7b54fdd438a86748d24870a1a7cd4f7d3a8f
Uday Sharma <udaysharmaprojects+git@gmail.com>
2026-05-01 23:59:00 +0530

    Updated Problem

--- Medium Problems/Merge_Sort_for_Linked_List.cpp
+++ Medium Problems/Merge_Sort_for_Linked_List.cpp
@@ -2,9 +2,7 @@
 Merge Sort for Linked List
 Given Pointer/Reference to the head of the linked list, the task is to Sort the given linked list using Merge Sort.
 Note: If the length of linked list is odd, then the extra node should go in the first list while splitting.
-
 Example 1:
-
 Input:
 N = 5
 value[]  = {3,5,2,4,1}
@@ -13,7 +11,6 @@ Explanation: After sorting the given
 linked list, the resultant matrix
 will be 1->2->3->4->5.
 Example 2:
-
 Input:
 N = 3
 value[]  = {9,15,0}
@@ -24,157 +21,80 @@ linked list , resultant will be
 Your Task:
 For C++ and Python: The task is to complete the function mergeSort() which sort the linked list using merge sort function.
 For Java: The task is to complete the function mergeSort() and return the node which can be used to print the sorted linked list.
-
 Expected Time Complexity: O(N*Log(N))
 Expected Auxiliary Space: O(N)
-
 Constraints:
 1 <= N <= 105
 */
 //{ Driver Code Starts
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
+/*
+class Node {
+ public:
     int data;
-    struct Node* next;
+    Node *next;
+
     Node(int x) {
         data = x;
         next = NULL;
     }
 };
-
-
-// } Driver Code Ends
-/* Structure of the linked list node is as
-struct Node 
-{
-    int data;
-    struct Node* next;
-    Node(int x) { data = x;  next = NULL; }
-};
 */
-
-
-class Solution{
-    private:
-    Node* findMid(Node* head) {
-    Node* slow = head;
-    Node* fast = head -> next;
-    
-    while(fast != NULL && fast -> next != NULL) {
-        slow = slow -> next;
-        fast = fast -> next -> next; 
-    }
-    return slow;
-}
-
-Node* merge(Node* left, Node* right) {
-    
-    if(left == NULL)
-        return right;
-    
-    if(right == NULL)
-        return left;
-    
-    Node* ans = new Node(-1);
-    Node* temp = ans;
-    
-    //merge 2 sorted Linked List
-    while(left != NULL && right != NULL) {
-        if(left -> data < right -> data ) {
-            temp -> next = left;
-            temp = left;
-            left = left -> next;
-        }
-        else
-        {
-            temp -> next = right;
-            temp = right;
-            right = right -> next;
-        }
-    }
-    
-    while(left != NULL) {
-        temp -> next = left;
-        temp = left;
-        left = left -> next;
-    }
-    
-    while(right != NULL) {
-        temp -> next = right;
-        temp = right;
-        right = right -> next;
-    }
-    
-    ans = ans -> next;
-    return ans;
-    
-}
+/*
+    Approach: Using the Merge Sort Algorithm to sort the linked list.
+    Time Complexity: O(max(m,n)) where m and n are the lengths of the two linked lists.
+    Space Complexity: O(1) as we are not using any extra space for sorting.
+*/
+class Solution {
   public:
-    //Function to sort the given linked list using Merge Sort.
-    Node* mergeSort(Node* head) {
-        // your code here
-            //base case
-    if( head == NULL || head -> next == NULL ) {
+    Node* sortedMerge(Node* head1, Node* head2) {
+        // code here
+        if(!head1 && !head2) return nullptr;
+        if(!head1) return head2;
+        if(!head2) return head1;
+        Node* res = new Node(-1);
+        Node* temp = res;
+        while(head2  && head1){
+            if(head1 ->data >head2->data){
+                temp->next = head2;
+                temp = head2;
+                head2 = head2->next;
+            }
+            else{
+                temp->next = head1;
+                temp = head1;
+                head1 =head1->next;
+            }
+        }
+        if(!head1) temp->next =head2;
+        else temp->next = head1;
+        return res->next;
+    }
+};
+/*
+    Approach: Using the Sort function of the vector to sort the elements and then create a new linked list with the sorted elements.
+    Time Complexity: O(N*Log(N)) where N is the number of nodes in the linked list.
+    Space Complexity: O(N) where N is the number of nodes in the linked list.
+*/
+class Solution {
+  public:
+    Node* sortedMerge(Node* head1, Node* head2) {
+        // code here
+        vector<int> ans;
+        while(head1) ans.push_back(head1->data),head1 = head1->next;
+        while(head2)ans.push_back(head2->data),head2 = head2->next;
+        sort(ans.begin(),ans.end());
+        Node* head =nullptr , *tail = nullptr;
+        for(int i:ans){
+            Node* node = new Node(i);
+            if(!head && !tail){
+                head = node;
+                tail = head;
+            }
+            else{
+                tail->next = node;
+                tail = node;
+            }
+        }
         return head;
     }
-    
-    // break linked list into 2 halvs, after finding mid
-    Node* mid = findMid(head);
-    
-    Node* left = head;
-    Node* right = mid->next;
-    mid -> next = NULL;
-    
-    //recursive calls to sort both halves
-    left = mergeSort(left);
-    right = mergeSort(right);
-    
-    //merge both left and right halves
-    Node* result = merge(left, right);
-    
-    return result;
-
-    }
 };
-
-
-//{ Driver Code Starts.
-
-void printList(Node* node) {
-    while (node != NULL) {
-        printf("%d ", node->data);
-        node = node->next;
-    }
-    printf("\n");
-}
-
-void push(struct Node** head_ref, int new_data) {
-    Node* new_node = new Node(new_data);
-
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
-}
-
-int main() {
-    long test;
-    cin >> test;
-    while (test--) {
-        struct Node* a = NULL;
-        long n, tmp;
-        cin >> n;
-        for (int i = 0; i < n; i++) {
-            cin >> tmp;
-            push(&a, tmp);
-        }
-        Solution obj;
-        a = obj.mergeSort(a);
-        printList(a);
-    }
-    return 0;
-}
-// } Driver Code Ends
