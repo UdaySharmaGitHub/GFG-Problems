4dd5fba6098aa5c50a7198e6e6a50e884edd5c89
Uday Sharma <udaysharmaprojects+git@gmail.com>
2026-06-18 23:57:18 +0530

    POTD 18-June-2026

--- Easy Problems/Coverage_of_all_Zeros_in_a_Binary_Matrix.cpp
+++ Easy Problems/Coverage_of_all_Zeros_in_a_Binary_Matrix.cpp
@@ -22,6 +22,59 @@ Constraints:
 1 <= matrix.size, matrix[0].size <= 100
 */
 
+/*
+    APPROACH
+    Steps:
+    1. We will iterate through the matrix and for each 0 we encounter, we will check its left, right, up and down neighbors to see if they are 1s. If they are, we will increment our total coverage count.
+    2. Finally, we will return the total coverage count.
+
+    Time Complexity: O(n * m) where n is the number of rows and m is the number of columns in the matrix.
+    Space Complexity: O(1) as we are not using any extra space.
+
+*/
+class Solution {
+  public:
+    int findCoverage(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size(), total = 0;
+        vector<bool> up(n, false), left(m, false),
+        right(m, false), down(n, false);
+        
+        for(int i=0; i<mat.size(); i++) {
+            for(int j=0; j<mat[i].size(); j++) {
+                if(mat[i][j] == 0) {
+                    if(left[i]) total++;
+                    if(up[j]) total++;
+                }
+                if(mat[i][j] == 1) {
+                    left[i] = true;
+                    up[j]   = true;
+                }
+                if(mat[m-i-1][n-j-1] == 0) {
+                    if(right[m-i-1]) total++;
+                    if(down[n-j-1]) total++;
+                }
+                if(mat[m-i-1][n-j-1] == 1) {
+                    right[m-i-1] = true;
+                    down[n-j-1] = true;
+                }
+            }
+        } return total;
+    }
+};
+
+/*
+    APPROACH
+    Steps:
+    1. We will maintain 4 boolean arrays to keep track of whether we have seen a 1 in the left, right, up and down direction for each row and column.   
+    2. We will iterate through the matrix from top-left to bottom-right and for each 0 we encounter, we will check if there is a 1 in the left and up direction using the left and up boolean arrays. If there is, we will increment our total coverage count.
+    3. We will also iterate through the matrix from bottom-right to top-left and for each 0 we encounter, we will check if there is a 1 in the right and down direction using the right and down boolean arrays. If there is, we will increment our total coverage count.
+    4. Finally, we will return the total coverage count.    
+
+    Time Complexity: O(n * m) where n is the number of rows and m is the number of columns in the matrix.
+    Space Complexity: O(n + m) for the boolean arrays used to keep track of the
+*/
+
+
 class Solution {
   public:
     int findCoverage(vector<vector<int>>& matrix) {
