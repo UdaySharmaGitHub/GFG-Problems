e4b65465f6c97f398d715e8801aed842edfc5bda
Uday Sharma <udaysharmaprojects+git@gmail.com>
2026-07-29 23:42:29 +0530

    POTD 29-07-2026 : Split Array into Minimum Subsets [Easy]

--- Easy Problems/Split_Array_into_Minimum_Subsets.cpp
+++ Easy Problems/Split_Array_into_Minimum_Subsets.cpp
@@ -32,6 +32,19 @@ Auxiliary Space: O(n)
 class Solution {
   public:
     int minSubsets(vector<int>& arr) {
+        // Your code here
+        sort(arr.begin(),arr.end());
+        int ans = 1;
+        for(int i = 1;i<arr.size();i++){
+            if(arr[i]!=arr[i-1]+1) ans++;
+        }
+        return ans;
+    }
+};
+/* --------------------------------------------------------------------------------------------------------- */
+class Solution {
+  public:
+    int minSubsets(vector<int>& arr) {
         // code here
         sort(arr.begin(),arr.end());
         int count = 1 ,compare = arr[0];
